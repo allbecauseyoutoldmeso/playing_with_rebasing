1807051099671a819ee1f938903f5707a77d4d1b
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-08-13 17:06:03 +0100

    edit file.rb

--- file.rb
+++ file.rb
@@ -1,7 +1,1 @@
-'this is the original string'
-
-'this is a string for the second commit to master'
-
-'this is the first edit on the first branch'
-
-'this is an edit on the master'
+'information in a file'
